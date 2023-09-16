c53662801f2576227047be4003f0bb4d8573dc24
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-09-16 21:00:54 +0600

    border layout

--- src/JFrameOne.java
+++ src/JFrameOne.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class JFrameOne extends JFrame {
     public JFrameOne() {
@@ -11,6 +12,15 @@ public class JFrameOne extends JFrame {
         setSize(500, 400);
         setLocationRelativeTo(null);
         setResizable(false);
+        BorderLayout borderLayout = new BorderLayout();
+        borderLayout.setHgap(15);
+        borderLayout.setVgap(20);
+        setLayout(borderLayout);
+        add(new Button("West"), BorderLayout.WEST);
+        add(new Button("North"), BorderLayout.NORTH);
+        add(new Button("South"), BorderLayout.SOUTH);
+        add(new Button("East"), BorderLayout.EAST);
+        add(new Button("Center"), BorderLayout.CENTER);
         setVisible(true);
     }
 }
