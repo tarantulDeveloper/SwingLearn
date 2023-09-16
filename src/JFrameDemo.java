6ccf36288cd334d7e0a10885a1bdb16246cb201c
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-09-16 20:20:18 +0600

    add JPanel

--- src/JFrameDemo.java
+++ src/JFrameDemo.java
@@ -1,6 +1,6 @@
 public class JFrameDemo {
     public static void main(String[] args) {
-        JFrameOne jFrameOne = new JFrameOne();
+//        JFrameOne jFrameOne = new JFrameOne();
         JFrameTwo jFrameTwo = new JFrameTwo();
     }
 }
--- src/JFrameTwo.java
+++ src/JFrameTwo.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class JFrameTwo {
     private JFrame frame;
@@ -14,6 +15,18 @@ public class JFrameTwo {
         this.frame.setSize(500, 400);
         this.frame.setLocationRelativeTo(null);
         this.frame.setResizable(false);
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.CYAN);
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 5));
+
+        Button button1 = new Button("Button 1");
+        panel.add(button1);
+        Button button2 = new Button("Button 2");
+        panel.add(button1);
+        Button button3 = new Button("Button 3");
+        panel.add(button1);
+
+        this.frame.add(panel, BorderLayout.SOUTH);
         this.frame.setVisible(true);
     }
 }
