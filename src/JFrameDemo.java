690e248416a17f7fd13c070bd2026a73b9c21c17
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-09-16 21:00:40 +0600

    comment the second

--- src/JFrameDemo.java
+++ src/JFrameDemo.java
@@ -1,6 +1,6 @@
 public class JFrameDemo {
     public static void main(String[] args) {
-//        JFrameOne jFrameOne = new JFrameOne();
-        JFrameTwo jFrameTwo = new JFrameTwo();
+        JFrameOne jFrameOne = new JFrameOne();
+//        JFrameTwo jFrameTwo = new JFrameTwo();
     }
 }
